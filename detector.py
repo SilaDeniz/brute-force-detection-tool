096e38549440c6b0afd2535a4586819f55525f36
Sila Deniz <sila.52.deniz@gmail.com>
2026-01-12 11:44:11 +0300

    Filter failed SSH login attempts

--- detector.py
+++ detector.py
@@ -1,9 +1,19 @@
 LOG_FILE = "sample_logs/auth.log"
 
-def read_log_file():
+def get_failed_logins():
+    failed_lines = []
+
     with open(LOG_FILE, "r") as file:
         for line in file:
-            print(line.strip())
+            if "Failed password" in line:
+                failed_lines.append(line.strip())
+
+    return failed_lines
+
 
 if __name__ == "__main__":
-    read_log_file()
+    failed_logins = get_failed_logins()
+
+    print("=== FAILED LOGIN ATTEMPTS ===")
+    for log in failed_logins:
+        print(log)
